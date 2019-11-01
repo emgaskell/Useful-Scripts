b5acc008812d021614a77bc4aeaa919eb1ab1281
SamJones1331 <55387388+SamJones1331@users.noreply.github.com>
2019-11-01 13:44:05 +0000

    Update and rename Docker_install.txt to Docker_install.sh

--- Docker_install.sh
+++ Docker_install.sh
@@ -1,6 +1,3 @@
 #curls the cript from a webpage and then passes it through bash
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
-restart shell/terminal
-# hello-world example
-docker run --rm hello-world
